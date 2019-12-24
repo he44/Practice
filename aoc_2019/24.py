d1cfbf5f0813f4e87677dd5499a6d12cdb909e2d
he44 <he44@illinois.edu>
2019-12-24 01:42:26 -0600

    Try an aoc

---
+++ aoc_2019/24.py
@@ -0,0 +1,10 @@
+"""
+Formulation
+a scan of the entire area fits into a 5x5 grid (your puzzle input). The scan shows bugs (#) and empty spaces (.).
+
+each tile updated every minute, simultaneously (counting for all first, then changing)
+
+Maybe need some bit operation: 25-bit number
+
+This way, it's easy to check if two layours are the same
+"""
