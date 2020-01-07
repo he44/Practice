f66f23ad22829e322a297d1dd3e65a3c8c798600
he44 <he44@illinois.edu>
2020-01-06 22:11:37 -0600

    Finishing off 1311 from last week's contest, BFS + some dictionary sorting

--- leetcode/20_0104_contest/5305.py
+++ leetcode/20_0104_contest/5305.py
@@ -1,3 +1,5 @@
+#  Problem 1311
+
 #  Run BFS to get friends at level k
 #  Use a ordered dictionary to store the video? 
 class Solution:
@@ -16,23 +18,31 @@ class Solution:
                     dist[friend] = dist[cur] + 1
         print(dist)
         #  Just do it the dumb way
-        import collections
-        videos = collections.OrderedDict()
+        #  manually sorting them...
+        videos_at_level_k = {}
+        for person in range(len(watchedVideos)):
+            #  check each person to see if they are level k
+            if dist[person] != level:
+                continue
+            #  append its video
+            for video in watchedVideos[person]:
+                if video not in videos_at_level_k:
+                    videos_at_level_k[video] = 1
+                else:
+                    videos_at_level_k[video] += 1
 
-        #  Find all frineds at level k
-        for friend in range(len(watchedVideos)):
-            if dist[friend] == level:
-                for video in watchedVideos[friend]:
-                    if video not in videos:
-                        videos[video] = 1
-                    else:
-                        videos[video] += 1
-        print(videos)
-        #  Sort out all the videos
-        #  sorted_videos = {k: v for k, v in sorted(videos.items(), key=lambda item: item[1])}
-        output = [k for k in videos]
-        output = output[::-1]
-        return output
+        print("Videos at level k %s"%videos_at_level_k)
+        #  Now we have to sort this dictionary, first by frequency, then alphabatically
+        #  this stack overflow post solved these two problems:
+        #  https://stackoverflow.com/questions/44076269/sort-counter-by-frequency-then-alphabetically-in-python
+        sorted_videos_at_level_k = sorted(videos_at_level_k.items(), key = lambda item: (item[1], item[0]))
+        videos = [x[0] for x in sorted_videos_at_level_k]
+        return videos
+                
+
+
+                
+            
         
 
 def main():
@@ -41,11 +51,15 @@ def main():
     friends = [[1,2],[0,3],[0,3],[1,2]]
     id = 0
     level = 2
+    output = s.watchedVideosByFriends(watchedVideos, friends, id, level)
+    print(output)
 
     watchedVideos = [["xk","qvgjjsp","sbphxzm"],["rwyvxl","ov"]]
     friends = [[1],[0]]
     id = 0
     level = 1
+    output = s.watchedVideosByFriends(watchedVideos, friends, id, level)
+    print(output)
 
     watchedVideos = [
         ["bjwtssmu"],
