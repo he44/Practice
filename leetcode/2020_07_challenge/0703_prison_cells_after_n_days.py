bd7f66ccd266bfa15cfa55d26d423e3c101d0b12
he44 <he44@illinois.edu>
2020-07-03 21:30:22 -0500

    finished 0703 leetcode

--- leetcode/2020_07_challenge/0703_prison_cells_after_n_days.py
+++ leetcode/2020_07_challenge/0703_prison_cells_after_n_days.py
@@ -2,35 +2,48 @@ from typing import *
 
 class Solution:
     def prisonAfterNDays(self, cells: List[int], N: int) -> List[int]:
+        # convert a cell configuration to an integer
         def cell_to_int(cells):
             res = 0
             for i in range(len(cells)-1, -1, -1):
-                res += 2 ** (len(cells) - i) * cells[i]
+                res += 2 ** (len(cells) - i - 1) * cells[i]
             return res
-        
-        pattern = cell_to_int(cells)
-        return pattern
 
-        prev_cells = cells
-        new_cells = [0 for x in range(len(cells))]
-        for i in range(N):
-            # so the first and last cells are always vacant
-            for j in range(1, len(cells)-1):
-                if prev_cells[j-1] == prev_cells[j+1]:
-                    #print("1", i, j)
-                    new_cells[j] = 1
-                else:
-                    #print("0", i, j)
-                    new_cells[j] = 0
-            # update
-            prev_cells = new_cells[:]
-            #print(i, new_cells, prev_cells)
+        def update_cells(cells):
+            new_cells = [0 for x in range(len(cells))]
+            for j in range(1, len(cells) - 1):
+                new_cells[j] = 1 if cells[j-1] == cells[j+1] else 0
+            return new_cells
+
+        seen = {}
+        new_cells = cells
+        i = 0
+        while i < N:
+            i += 1
+            new_cells = update_cells(new_cells)
+            new_cells_pattern = cell_to_int(new_cells)
+            if new_cells_pattern in seen:
+                cyc_len = i - seen[new_cells_pattern]
+                num_cycs = (N - i) // cyc_len
+                i += num_cycs * cyc_len
+            else:
+                seen[new_cells_pattern] = i
+
         return new_cells
 
 
+
 cases = [
-    [[0,1,0,1,1,0,0,1], 2],
-    [[0,1,0,1,1,0,0,1], 7]
+    [[0, 1, 0, 1, 1, 0, 0, 1], 7]#,
+    # [[0, 1, 0, 1, 1, 0, 0, 1], 14],
+    # [[0, 1, 0, 1, 1, 0, 0, 1], 15],
+    # [[0, 1, 0, 1, 1, 0, 0, 1], 100],
+    # [[0, 1, 0, 1, 1, 0, 0, 1], 100000],
+    # [[1, 0, 0, 1, 0, 0, 1, 0], 1000000000]
+]
+
+cases = [
+    [[0, 0, 0, 1, 1, 0, 1, 0], 574]
 ]
 
 sol = Solution()
