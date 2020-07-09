33027ea70501b061201789ca4dcd04db3d4accdb
he44 <he44@illinois.edu>
2020-07-08 23:30:31 -0500

    0703 well

--- leetcode/2020_07_challenge/0708_3_sum.py
+++ leetcode/2020_07_challenge/0708_3_sum.py
@@ -3,38 +3,24 @@ from typing import *
 class Solution:
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         n = len(nums)
-        sum2_dict = {}
-        for i in range(n):
-            for j in range(i+1, n):
-                need = -(nums[i] + nums[j])
-                pair = (i, j) # keep the index, not the number; already sorted
-                if need in sum2_dict:
-                    sum2_dict[need].append(pair)
-                else:
-                    sum2_dict[need] = [pair]
-
-        res = {}
-        for i in range(n):
-            z = nums[i]
-            if z in sum2_dict:
-                for x, y in sum2_dict[z]:
-                    if i == x or i == y:
-                        continue
-                    trip = []
-                    rx = min(nums[x], nums[y])
-                    ry = max(nums[x], nums[y])
-                    if z < rx:
-                        trip = (z, rx, ry)
-                    elif z > ry:
-                        trip = (rx, ry, z)
-                    else:
-                        trip = (rx, z, ry)
-                    if trip not in res:
-                        res[trip] = 1
-        return [[x[0], x[1], x[2]] for x in res]
-
-
-
+        res = []
+        found = set()
+        dups = set()
+        seen = {}
+        for i, val1 in enumerate(nums):
+            if val1 in dups:
+                continue
+            dups.add(val1)
+            for j, val2 in enumerate(nums[i+1:]):
+                complement = -(val1 + val2)
+                if complement in seen and seen[complement] == i:
+                    rmin = min(val1, val2, complement)
+                    rmax = max(val1, val2, complement)
+                    if (rmin, rmax) not in found:
+                            found.add((rmin, rmax))
+                            res.append([val1, val2, complement])
+                seen[val2] = i
+        return res
 
 
 cases = [
