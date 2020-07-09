38b7b3178c792a85d4857cc95fa6f64366a4c15b
he44 <johnwatson221bbaker@outlook.com>
2020-07-09 11:49:13 -0500

    0709

--- leetcode/2020_07_challenge/0709_maximum_width_binary_tree.py
+++ leetcode/2020_07_challenge/0709_maximum_width_binary_tree.py
@@ -2,4 +2,18 @@ from typing import *
 
 class Solution:
     def widthOfBinaryTree(self, root: TreeNode) -> int:
-        pass
+        max_width = 0
+        queue = [(root, 1)]
+        while queue:
+            level_len = len(queue)
+            level_head = queue[0][1]
+            #print(level_len, level_head, queue)
+            for _ in range(level_len):
+                cur, cur_col = queue.pop(0)
+                max_width = max(max_width, cur_col - level_head + 1)
+                #print(cur.val, cur_col)
+                if cur.left:
+                    queue.append((cur.left, cur_col * 2))
+                if cur.right:
+                    queue.append((cur.right, cur_col * 2 + 1))
+        return max_width
