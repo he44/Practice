aaf6c50af7cde88f270fef58f9f3613ea64bca67
he44 <he44@illinois.edu>
2020-07-29 01:59:04 -0500

    some codeforces

--- codeforces/50A.cpp
+++ codeforces/50A.cpp
@@ -1,4 +1,12 @@
+#include <iostream>
+#include <vector>
 
-int main(){
-    return 0;
+int main()
+{
+	int M, N;
+	std::cin >> M >> N;
+	int good_num = M / 2;
+	int bad_num = N / 2;
+	int ans = M % 2 == 0?good_num * N:good_num * N + bad_num;
+	std::cout << ans << std::endl;
 }
